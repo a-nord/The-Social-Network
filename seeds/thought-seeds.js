010330dedb0081f5a6baf65eeac966885035f72e
Ashley Nord <anord@hntb.com>
2024-03-01 01:18:53 -0500

    updated the thoughts seeds with quotes

--- seeds/thought-seeds.js
+++ seeds/thought-seeds.js
@@ -4,8 +4,58 @@ const seedThoughts = [
     username: "booklover99",
     reactions: [
       {
-        reactionBody: "👍",
+        reactionBody: "😢",
         username: "dungsANDdrags",
+      },
+      {
+        reactionBody: "😞",
+        username: "bookworm22",
+      },
+      {
+        reactionBody: "👎",
+        username: "jonsnowsdoppleganger",
+      },
+    ],
+  },
+  {
+    thoughtText: "“I'd cut up my heart for you to wear if you wanted it.” ― Margaret Mitchell, Gone with the Wind",
+    username: "dungsANDdrags",
+    reactions: [
+      {
+        reactionBody: "😓",
+        username: "bookworm22",
+      },
+      {
+        reactionBody: "😥",
+        username: "booklover99",
+      },
+      {
+        reactionBody: "🤢",
+        username: "jonsnowsdoppleganger",
+      },
+    ],
+  },
+  {
+    thoughtText: "“A lady's imagination is very rapid; it jumps from admiration to love, from love to matrimony in a moment.” ― Jane Austen, Pride and Prejudice",
+    username: "bookworm22",
+    reactions: [
+      {
+        reactionBody: "😌",
+        username: "booklover99",
+      },
+      {
+        reactionBody: "😚",
+        username: "dungsANDdrags",
+      },
+    ],
+  },
+  {
+    thoughtText: "“What people lack in life is not more reality but illusion, fantasy, play.” ― Robert Greene, The Art of Seduction",
+    username: "jonsnowsdoppleganger",
+    reactions: [
+      {
+        reactionBody: "😾",
+        username: "booklover99",
       },
       {
         reactionBody: "😄",
@@ -14,8 +64,8 @@ const seedThoughts = [
     ],
   },
   {
-    thoughtText: "This is a sample thought about something else interesting.",
-    username: "dungsANDdrags",
+    thoughtText: "“Maybe you are amazing and beautiful. I just can't see it anymore.” ― John Casey, Raw Thoughts",
+    username: "booklover99",
     reactions: [
       {
         reactionBody: "👍",
@@ -23,12 +73,12 @@ const seedThoughts = [
       },
       {
         reactionBody: "😄",
-        username: "booklover99",
+        username: "dungsANDdrags",
       },
     ],
   },
   {
-    thoughtText: "This is a sample thought about something else interesting.",
+    thoughtText: "“Reveal to me everything you find ugly about yourself, so I can reveal to you everything I find beautiful about you.” ― Juansen Dizon, Confessions of a Wallflower",
     username: "bookworm22",
     reactions: [
       {
@@ -42,72 +92,30 @@ const seedThoughts = [
     ],
   },
   {
-    thoughtText: "This is a sample thought about something else interesting.",
+    thoughtText: "“I have hunger for your mouth, for your voice, for your hair” ― Pablo Neruda, 100 Love Sonnets",
+    username: "dungsANDdrags",
+    reactions: [
+      {
+        reactionBody: "👍",
+        username: "booklover99",
+      },
+      {
+        reactionBody: "😄",
+        username: "bookworm22",
+      },
+    ],
+  },
+  {
+    thoughtText: "“A man grows bored with a woman, no matter how beautiful; he yearns for different pleasures, and for adventure.” ― Robert Greene, The Art of Seduction",
     username: "jonsnowsdoppleganger",
     reactions: [
       {
         reactionBody: "👍",
-        username: "Yennefer",
+        username: "booklover99",
       },
       {
         reactionBody: "😄",
-        username: "Triss",
-      },
-    ],
-  },
-  {
-    thoughtText: "This is a sample thought about something else interesting.",
-    username: "Yennefer",
-    reactions: [
-      {
-        reactionBody: "👍",
-        username: "Triss",
-      },
-      {
-        reactionBody: "😄",
-        username: "Ciri",
-      },
-    ],
-  },
-  {
-    thoughtText: "This is a sample thought about something else interesting.",
-    username: "Triss",
-    reactions: [
-      {
-        reactionBody: "👍",
-        username: "Yennefer",
-      },
-      {
-        reactionBody: "😄",
-        username: "Ciri",
-      },
-    ],
-  },
-  {
-    thoughtText: "This is a sample thought about something else interesting.",
-    username: "Ciri",
-    reactions: [
-      {
-        reactionBody: "👍",
-        username: "Yennefer",
-      },
-      {
-        reactionBody: "😄",
-        username: "Triss",
-      },
-    ],
-  },
-  {
-    thoughtText: "This is a sample thought about something else interesting.",
-    username: "Geralt",
-    reactions: [
-      {
-        reactionBody: "👍",
-        username: "Yennefer",
-      },
-      {
-        reactionBody: "😄",
-        username: "Triss",
+        username: "bookworm22",
       },
     ],
   },
